b8654b75050673b632b79dcd1012c432a3e4ad89
rahulrameshm0 <iamrahulm23@gmail.com>
2025-10-17 20:22:41 +0530

    codebase updated

--- model window/script.js
+++ model window/script.js
@@ -1,5 +1,21 @@
-const show =  document.querySelector('.model');
-const close_model = document.querySelector('.close-model');
+const modal =  document.querySelector('.modal');
+const close = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.window-1');
+const btn = document.querySelectorAll('.window-1');
 console.log(btn) 
+
+const open_modal = function () {
+    modal.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+}
+
+const close_modal = function () {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btn.length; i++) btn[i].addEventListener('click', open_modal);
+
+close.addEventListener('click', close_modal);
+
+overlay.addEventListener('click', close_modal);
