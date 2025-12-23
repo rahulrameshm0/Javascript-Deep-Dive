cd5a611ef439a07852ec10beae4d4682e4c11558
rahulrameshm0 <iamrahulm23@gmail.com>
2025-12-24 00:20:55 +0530

    implemented lazy loading images

--- bankist website/script.js
+++ bankist website/script.js
@@ -6,6 +6,11 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav')
+const tabs = document.querySelectorAll('.operations__tab');
+const tabContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 
 ///////////////////////////////////////
 // Modal window
@@ -133,6 +138,7 @@ document.querySelector('.nav__links').addEventListener(
 
 // Non-standard
 
+
 // console.log(logo.designer);
 // console.log(logo.getAttribute('designer'));
 // logo.setAttribute('company', 'Bankist');
@@ -210,17 +216,17 @@ console.log(h1.querySelector('.highlight'));
 console.log(h1.childNodes);
 console.log(h1.children);
 
-h1.firstElementChild.style.color = "white";
-h1.lastElementChild.style.color = "orangered";
+// h1.firstElementChild.style.color = "white";
+// h1.lastElementChild.style.color = "orangered";
 
 
 // Going upwards: parents
 
-console.log(h1.parentNode);
-console.log(h1.parentElement);
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
-h1.closest('.header').style.background = 'var(--gradient-secondary)';
-h1.closest('h1').style.background = 'var(--gradient-primary)';
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// h1.closest('h1').style.background = 'var(--gradient-primary)';
 
 // Going sideways: siblings
 
@@ -234,3 +240,101 @@ console.log(h1.nextElementSibling);
 // [...h1.parentElement.children].forEach(function(el){
 //   if(el !== h1) el.style.transform = 'scale(0.5)'
 // });
+
+
+tabContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.operations__tab');
+  
+  if (!clicked) return; 
+  
+
+  tabs.forEach(t => t.classList.remove
+    ('operations__tab--active'));
+  
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  
+  
+    clicked.classList.add('operations__tab--active');
+
+  
+  // Active Content Area
+document
+  .querySelector(`.operations__content--${clicked.dataset.tab}`)
+  .classList.add('operations__content--active');
+  
+});
+
+const handleHover = function(e){
+    if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if(el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing an argument into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// stcky navigation
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function(e){
+  // console.log(this.window.scrollY)
+
+  if (window.scrollY > initialCoords.top) 
+    nav.classList.add('sticky'); 
+  else nav.classList.add('sticky')
+});
+
+
+// Reveal seciton
+const allSection = document.querySelectorAll('.section')
+const revealSection = function(entries, observer){
+  const [entry] = entries;
+  // console.log(entry);
+  if(!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root:null,
+  threshold:0.15
+});
+
+
+allSection.forEach(function(section){
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden')
+});
+
+// Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function(entries, obsever){
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  // replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+  entry.target.classList.remove('lazy-img');
+  entry.target.addEventListner('load', function(){});
+  obsever.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg,
+  { root:null,
+  threshold: 0,
+  rootMargin: '-200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+// console.log(imgTargets); 
