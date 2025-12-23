36c28dfdf70d3401687316547f00ec75e0c6bb88
rahulrameshm0 <iamrahulm23@gmail.com>
2025-12-23 13:35:14 +0530

    code updated

--- bankist website/script.js
+++ bankist website/script.js
@@ -202,3 +202,35 @@ document.querySelector('.nav__links').addEventListener(
 
 
 // console.log(randomColor(0,255));
+
+
+// Going downwords: child
+const h1 = document.querySelector('h1');
+console.log(h1.querySelector('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "orangered";
+
+
+// Going upwards: parents
+
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+// console.log(h1.nextSibling);
+// console.log(h1.previousSibling);
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function(el){
+//   if(el !== h1) el.style.transform = 'scale(0.5)'
+// });
