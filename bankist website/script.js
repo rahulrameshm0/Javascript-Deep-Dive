36e92c6bbc259f9cdddeddbc2897ce9f5efce199
rahulrameshm0 <iamrahulm23@gmail.com>
2025-12-24 20:31:36 +0530

    added slider

--- bankist website/script.js
+++ bankist website/script.js
@@ -338,3 +338,46 @@ const imgObserver = new IntersectionObserver(loadImg,
 
 imgTargets.forEach(img => imgObserver.observe(img));
 // console.log(imgTargets); 
+
+
+// slider
+
+const slides = document.querySelectorAll('.slide');
+// const slider = document.querySelector('.slider');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const maxSlide = slides.length;
+let currentSlide = 0;
+
+// slider.style.transform = 'scale(0.4) translateX(-800px)';
+// slider.style.overflow = 'visible';
+
+const goToSlide = function(slide){
+  slides.forEach((s, i) => s.style.transform =  
+  `translateX(${100 * (i - slide)}%)`);
+}
+
+goToSlide(0)
+
+// Next Slide
+
+const nextSlide = function(){
+   if (currentSlide === maxSlide - 1){
+      currentSlide = 0;
+  }else{
+    currentSlide++;
+  }
+  goToSlide(currentSlide);
+}
+
+const prevSlide = function(){
+  if (currentSlide === 0){
+    currentSlide = maxSlide - 1;
+  }else{
+    currentSlide--;
+  }
+  goToSlide(currentSlide);
+};
+
+btnRight.addEventListener('click',nextSlide);
+btnLeft.addEventListener('click', prevSlide);
